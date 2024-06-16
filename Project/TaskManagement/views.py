d60d1c7cdb2f29e8120d1ef83be989f83a0406ac
Sangeetha <sangeethapa2002@gmail.com>
2024-06-16 22:10:45 +0530

    added 1. Retrieve a specific task for the specific user 2. Delete a specific task for specific user

--- Project/TaskManagement/views.py
+++ Project/TaskManagement/views.py
@@ -2,20 +2,53 @@ from django.shortcuts import render, redirect
 from django.http import JsonResponse
 import json
 from django.views.decorators.csrf import csrf_exempt
+from django.shortcuts import get_object_or_404
 # Create your views here.
 from TaskManagement.models import Task, Users
+
+
 
 @csrf_exempt
 def update_task(request, user_id, task_id):
     if request.method == "PUT":
         print("Get Inside")
         data = json.loads(request.body)
-        task_exist = Task.objects.filter(user_id=user_id, id=task_id).exists()
-        if task_exist == True:
+        try:    
+            task_exist = get_object_or_404(Task, user_id=user_id, id = task_id)
+        
             update = Task.objects.filter(user_id=user_id, id=task_id).update(status = data['status'])
-            return JsonResponse({"msg" : update})
-        else:
+            return JsonResponse({"msg" : "Updated Successfully"})
+        except Exception:
             return JsonResponse({"msg" : "No user_id found"})
+    elif request.method == "GET":
+        print("Retrieve Data")
+        try:
+            task_exist = get_object_or_404(Task, user_id=user_id, id = task_id)
+            print(task_exist)
+            detail = {
+                'id' : task_exist.id,
+                'title': task_exist.title,
+                'description': task_exist.description,
+                'due_date': task_exist.due_date,
+                'status': task_exist.status,
+                'user_id': task_exist.user_id
+            }
+            return JsonResponse(detail)
+        except Exception as e:
+            return JsonResponse({'error': str(e)}, status=500)
+    
+    elif request.method == "DELETE":
+        try:
+            task_exist = get_object_or_404(Task, user_id=user_id, id = task_id)
+            task_exist.delete()
+            return JsonResponse({'msg' : 'Task Deleted Successfully'})
+        except:
+            return JsonResponse({'msg':"No Task Found"})
+
+
+
+
+
 
 
 @csrf_exempt
@@ -31,11 +64,13 @@ def create_task(request, user_id):
         description = data['description']
         due_date = data['due_date']
         status = data['status']
-        task_exists = Task.objects.filter(title=title, user_id=user_id).exists()
-        print(task_exists)
-        if task_exists == True:
-            return JsonResponse({"msg": "Task Already assigned"})
+
+        
         try:
+            task_exists = Task.objects.filter(title=title, user_id=user_id).exists()
+
+            if task_exists == True:
+                return JsonResponse({"msg": "Task Already assigned"})
             user = Task.objects.create(title=title, description=description, due_date=due_date,status=status, user_id=user_id)
             user.save()
             print(title, description, due_date, status)
@@ -43,19 +78,22 @@ def create_task(request, user_id):
         except Exception as e:
             return JsonResponse({'error': str(e)}, status=400)
     elif request.method == "GET":
-        user_id_exists = Task.objects.filter(user_id=user_id)
-        check = user_id_exists.exists()
-        print(Task.objects.all())
-        if check == True:
-            task_list = list(user_id_exists.values())
-            print("Details : ", task_list)
-            details = {}
-            for i in range(len(task_list)):
-                details[i] = task_list[i]
 
-            #task_list = list(details.values())
-            return JsonResponse(details)
-        else:
+        try:
+            user_id_exists = Task.objects.filter(user_id=user_id)
+            check = user_id_exists.exists()
+            if check == True:
+                task_list = list(user_id_exists.values())
+                print("Details : ", task_list)
+                details = {}
+                for i in range(len(task_list)):
+                    details[i] = task_list[i]
+
+                #task_list = list(details.values())
+                return JsonResponse(details)
+            else:
+                return JsonResponse({'msg' : "User_id not found"})  
+        except:
             return JsonResponse({'msg' : "User_id not found"})        
         
 @csrf_exempt        
